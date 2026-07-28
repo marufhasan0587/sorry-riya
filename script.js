fddeb618b3cdcc509e2668e7568ad46f9e0cb8ba
marufhasan0587 <marufiiii0155@gmail.com>
2026-07-29 01:38:48 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const text = "Sometimes, a simple 'Sorry' carries more truth than a thousand explanations...";
+let i = 0;
 
+function typingEffect() {
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typingEffect, 50);
+    }
+}
+
+window.onload = typingEffect;
+
+function openLetter() {
+    document.getElementById("letter").style.display = "block";
+    document.querySelector("button").style.display = "none";
+
+    document.getElementById("letter").scrollIntoView({
+        behavior: "smooth"
+    });
+}
